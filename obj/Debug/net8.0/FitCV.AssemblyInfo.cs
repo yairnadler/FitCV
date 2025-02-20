e3f9a70854af944f5de8b198e5b8fa56061f8775
Yair Nadler <yair.nadler7@gmail.com>
2025-02-21 01:24:24 +0200

    rm .env

--- obj/Debug/net8.0/FitCV.AssemblyInfo.cs
+++ obj/Debug/net8.0/FitCV.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitCV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b487117cf8b3589265a324760728348901346d67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0a1d708e2f23027085709ef76eab519f0fcdc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitCV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitCV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
