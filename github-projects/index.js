2374cb835817ca6505eeb24f5aca11d1ece60114
Yair Nadler <yair.nadler7@gmail.com>
2025-02-25 21:46:32 +0200

    changed to microservices approach

--- github-projects/index.js
+++ github-projects/index.js
@@ -1,7 +1,5 @@
 import express, { json } from "express";
 import morgan from "morgan";
-import puppeteer from "puppeteer";
-import { GoogleGenerativeAI } from "@google/generative-ai";
 import { Octokit } from "@octokit/rest";
 import axios from "axios";
 import dotenv from "dotenv";
@@ -9,50 +7,15 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-const PORT = 3000;
+const PORT = 5001;
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const octokit = new Octokit({ auth: GITHUB_TOKEN });
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
-  systemInstruction: `You are a software engineer headhunter!`,
-});
 
 // Middlewares
 app.use(json());
 app.use(morgan("dev"));
 
 const githubRepos = [];
-const linkedinJobSkills = [];
-
-const linkedinScrape = async function (url, cssSelector) {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-
-  await page.goto(url);
-
-  const jobDescription = await page.evaluate((selector) => {
-    const element = document.querySelector(selector);
-    return element.textContent;
-  }, cssSelector);
-
-  await browser.close();
-
-  if (!jobDescription) {
-    return [];
-  }
-
-  const prompt = `Here is a job description for a software engineer position from a linkedin job posting:
-  ${jobDescription}
-  List the relevant technical skills with this schema: { ["skill": str, "description": str] }`;
-
-  const result = await model.generateContent(prompt);
-  const resultText = result.response.text();
-  const cleanedText = resultText.replace(/```json|```/g, '').trim();
-  const requiredSkills = JSON.parse(cleanedText);
-
-  return requiredSkills;
-};
 
 const githubScrape = async function (githubUsername) {
   const reposUrl = `https://api.github.com/users/${githubUsername}/repos`;
@@ -113,41 +76,6 @@ const fetchReadmeFile = async function (githubUsername, githubRepo) {
   }
 };
 
-app.get("/api", (req, res) => {
-  res.send("Hello World");
-});
-
-app.post("/api/linkedin-jobs/:id", async (req, res) => {
-  const jobId = req.params.id;
-  const liknedinScrapeUrl = `https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/${jobId}`;
-  const cssSelector = "[class*=description] > section > div";
-
-  const requiredSkills = await linkedinScrape(liknedinScrapeUrl, cssSelector);
-  linkedinJobSkills[jobId] = [];
-
-  requiredSkills.forEach((skill) => {
-    if (!linkedinJobSkills.includes(skill)) {
-      linkedinJobSkills[jobId].push(skill);
-    }
-  });
-
-  if (requiredSkills.length === 0) {
-    return res.status(404).send("No skills found");
-  }
-
-  return res.status(200).send(requiredSkills);
-});
-
-app.get("/api/linkedIn-jobs/:id", async (req, res) => {
-  const jobId = req.params.id;
-  if (!linkedinJobSkills[jobId]) {
-    return res.status(404).send("No skills found");
-  }
-
-  return res.status(200).send(linkedinJobSkills[jobId]);
-});
-
-
 app.get("/api/github/projects", async (req, res) => {
   return res.status(200).send(githubRepos);
 });
@@ -189,6 +117,6 @@ app.post("/api/github/projects", async (req, res) => {
   return res.status(200).send(githubRepos);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`github-microservice server is running on port ${PORT}`);
 });
